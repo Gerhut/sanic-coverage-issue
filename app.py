9718ba1242e7d9e0d95db777f21ab143c347301a
George Cheng <Gerhut@GMail.com>
2024-12-31 08:24:04 +0000

    finished

--- app.py
+++
@@ -1,9 +0,0 @@
-from sanic import Sanic, text, Request
-
-app = Sanic(__name__)
-
-@app.get('/')
-def index(_):
-    return text('Hello, World!')
-
-__all__ = ['app']
--- test_app.py
+++ test_app.py
@@ -1,19 +1,8 @@
-import pytest
-from sanic_testing.testing import SanicTestClient
+def test_app():
+    from sanic import Sanic
 
-
-@pytest.fixture
-def client():
-    from app import app
-
-    app.config.TOUCHUP = False
-
-    return app.test_client
-
-
-def test_app(client: SanicTestClient):
-    _, response = client.get('/')
-
+    app = Sanic(__name__)
+    client = app.test_client
+    _, response = client.get("/")
     assert response is not None
-    assert response.status == 200
-    assert response.text == 'Hello, World!'
+    assert response.status == 500
